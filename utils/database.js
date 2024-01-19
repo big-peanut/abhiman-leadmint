5f330437422ebf644484afba9a5ba151525fe4dc
Sandeep Sundar Lenka <82632232+big-peanut@users.noreply.github.com>
2024-01-19 22:32:51 +0530

    Update database.js

--- utils/database.js
+++ utils/database.js
@@ -1,21 +1,38 @@
-// Import the Sequelize library for database interactions
-const Sequelize = require("sequelize");
+// // Import the Sequelize library for database interactions
+// const Sequelize = require("sequelize");
 
-// Load environment variables from a .env file
+// // Load environment variables from a .env file
+// const dotenv = require("dotenv");
+// dotenv.config();
+
+// // Create a new Sequelize instance for connecting to the database
+// const sequelize = new Sequelize(
+//   // Use environment variables for database configuration
+//   process.env.DB_NAME, // Database name
+//   process.env.DB_USER, // Database user
+//   process.env.DB_PASSWORD, // Database password
+//   {
+//     dialect: "mysql", // Specify the database dialect (MySQL in this case)
+//     hostname: process.env.DB_HOST, // Use environment variable for the database host
+//   }
+// );
+
+// // Export the Sequelize instance to be used in other parts of the application
+// module.exports = sequelize;
+
+const mysql = require("mysql2/promise");
 const dotenv = require("dotenv");
 dotenv.config();
 
-// Create a new Sequelize instance for connecting to the database
-const sequelize = new Sequelize(
-  // Use environment variables for database configuration
-  process.env.DB_NAME, // Database name
-  process.env.DB_USER, // Database user
-  process.env.DB_PASSWORD, // Database password
-  {
-    dialect: "mysql", // Specify the database dialect (MySQL in this case)
-    hostname: process.env.DB_HOST, // Use environment variable for the database host
-  }
-);
+// Create a connection pool
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
 
-// Export the Sequelize instance to be used in other parts of the application
-module.exports = sequelize;
+console.log("pool created");
+
+// Export the pool for use in other parts of the application
+module.exports = pool;
