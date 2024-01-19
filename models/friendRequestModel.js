c16c6db4ddb5f4098b0d2145b7de8b1e702ec5c4
Sandeep Sundar Lenka <82632232+big-peanut@users.noreply.github.com>
2024-01-19 22:35:54 +0530

    Update friendRequestModel.js

--- models/friendRequestModel.js
+++ models/friendRequestModel.js
@@ -1,48 +1,79 @@
-// Import Sequelize for defining models
-const Sequelize = require("sequelize");
+// // Import Sequelize for defining models
+// const Sequelize = require("sequelize");
 
-// Import the sequelize instance for connecting to the database
-const sequelize = require("../utils/database");
+// // Import the sequelize instance for connecting to the database
+// const sequelize = require("../utils/database");
 
-// Import the Users model for reference in the friendrequests table
-const Users = require("./userModel");
+// // Import the Users model for reference in the friendrequests table
+// const Users = require("./userModel");
+
+// // Define the Friends model
+// const Friends = sequelize.define("friendrequests", {
+//   // Define columns for the friendrequests table
+//   id: {
+//     type: Sequelize.INTEGER,
+//     autoIncrement: true,
+//     allowNull: false,
+//     primaryKey: true,
+//   },
+//   senderId: {
+//     type: Sequelize.INTEGER,
+//     allowNull: false,
+//   },
+//   receiverId: {
+//     type: Sequelize.INTEGER,
+//     allowNull: false,
+//   },
+//   areFriends: {
+//     type: Sequelize.BOOLEAN,
+//     defaultValue: false,
+//     allowNull: false,
+//   },
+// });
+
+// // Define relationships between Users and Friends
+// Users.belongsToMany(Users, {
+//   as: "Sender",
+//   foreignKey: "senderId",
+//   through: Friends,
+// });
+
+// Users.belongsToMany(Users, {
+//   as: "Receiver",
+//   foreignKey: "receiverId",
+//   through: Friends,
+// });
+
+// // Export the Friends model for use in other parts of the application
+// module.exports = Friends;
+
+const pool = require("../utils/database");
+
+// Define the table name
+const tableName = "friendrequests";
 
 // Define the Friends model
-const Friends = sequelize.define("friendrequests", {
-  // Define columns for the friendrequests table
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true,
+const Friends = {
+  // Function to create the friendrequests table
+  createTable: async () => {
+    const createTableQuery = `
+      CREATE TABLE IF NOT EXISTS ${tableName} (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        senderId INT NOT NULL,
+        receiverId INT NOT NULL,
+        areFriends BOOLEAN DEFAULT false NOT NULL
+      )
+    `;
+    try {
+      const [rows, fields] = await pool.query(createTableQuery);
+      console.log("Friends table created:");
+    } catch (error) {
+      console.error("Error creating Friends table:", error);
+    }
   },
-  senderId: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
-  },
-  receiverId: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
-  },
-  areFriends: {
-    type: Sequelize.BOOLEAN,
-    defaultValue: false,
-    allowNull: false,
-  },
-});
 
-// Define relationships between Users and Friends
-Users.belongsToMany(Users, {
-  as: "Sender",
-  foreignKey: "senderId",
-  through: Friends,
-});
-
-Users.belongsToMany(Users, {
-  as: "Receiver",
-  foreignKey: "receiverId",
-  through: Friends,
-});
+  // Add other model-related functions here
+};
 
 // Export the Friends model for use in other parts of the application
 module.exports = Friends;
