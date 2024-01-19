2f102b6728acfe37dab84cbcef0e06a2f14c6212
Sandeep Sundar Lenka <82632232+big-peanut@users.noreply.github.com>
2024-01-19 22:39:23 +0530

    Update primeController.js

--- controllers/primeController.js
+++ controllers/primeController.js
@@ -1,39 +1,90 @@
-// Import the Users model
-const Users = require("../models/userModel");
+// // Import the Users model
+// const Users = require("../models/userModel");
 
-// Function to enable a user to purchase a prime membership
+// // Function to enable a user to purchase a prime membership
+// exports.buyPrime = async (req, res, next) => {
+//   try {
+//     // Update the user's isPrime status to true
+//     await Users.update(
+//       { isPrime: true },
+//       { where: { userId: req.user.userId } }
+//     );
+//     res.json({ message: "Prime membership purchased successfully" });
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+
+// // Function to deduct coins from a user's account
+// exports.deductCoins = async (req, res, next) => {
+//   try {
+//     // Extract the amount of coins to be deducted from the request body
+//     const amount = req.body.coins;
+
+//     // Find the user in the database
+//     const user = await Users.findOne({ where: { id: req.user.id } });
+
+//     // Check if the user has sufficient coins
+//     if (user.coins < amount) {
+//       return res
+//         .status(400)
+//         .json({ success: false, msg: "Insufficient coins" });
+//     }
+
+//     // Deduct the specified amount of coins from the user's account
+//     user.coins -= amount;
+//     await user.save();
+
+//     // Return success message in the response
+//     return res
+//       .status(200)
+//       .json({ success: true, msg: "Coins deducted successfully" });
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+
+const pool = require("../utils/database");
+
 exports.buyPrime = async (req, res, next) => {
   try {
     // Update the user's isPrime status to true
-    await Users.update(
-      { isPrime: true },
-      { where: { userId: req.user.userId } }
-    );
+    const buyPrimeQuery = `
+      UPDATE users
+      SET isPrime = true
+      WHERE userId = ?
+    `;
+    await pool.query(buyPrimeQuery, [req.user.userId]);
+
     res.json({ message: "Prime membership purchased successfully" });
   } catch (err) {
     console.log(err);
   }
 };
 
-// Function to deduct coins from a user's account
 exports.deductCoins = async (req, res, next) => {
   try {
     // Extract the amount of coins to be deducted from the request body
     const amount = req.body.coins;
 
     // Find the user in the database
-    const user = await Users.findOne({ where: { id: req.user.id } });
+    const getUserQuery = "SELECT * FROM users WHERE id = ?";
+    const [user] = await pool.query(getUserQuery, [req.user.id]);
 
     // Check if the user has sufficient coins
-    if (user.coins < amount) {
+    if (user[0].coins < amount) {
       return res
         .status(400)
         .json({ success: false, msg: "Insufficient coins" });
     }
 
     // Deduct the specified amount of coins from the user's account
-    user.coins -= amount;
-    await user.save();
+    const deductCoinsQuery = `
+      UPDATE users
+      SET coins = coins - ?
+      WHERE id = ?
+    `;
+    await pool.query(deductCoinsQuery, [amount, req.user.id]);
 
     // Return success message in the response
     return res
